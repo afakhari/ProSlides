be5daffdb4d9bfddd17bd9db38db66ffb6d9816d
afakhari <hagamirali@gmail.com>
2025-10-24 17:22:28 +0330

    Add  DRF and django_filters in settings installed_apps

--- backend/srvs/office/office/settings.py
+++ backend/srvs/office/office/settings.py
@@ -7,9 +7,11 @@ env.read_env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Security
-SECRET_KEY = env.str("SECRET_KEY", default='django-insecure-82s@p*pkocb%%s6cvx_irf%=8bpa2_xgge825ixjf(jro%84%q')
+SECRET_KEY = env.str(
+    "SECRET_KEY", default='django-insecure-82s@p*pkocb%%s6cvx_irf%=8bpa2_xgge825ixjf(jro%84%q')
 DEBUG = env.bool("DEBUG", default=True)
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default="127.0.0.1,0.0.0.0,localhost")
+ALLOWED_HOSTS = env.list(
+    "ALLOWED_HOSTS", default="127.0.0.1,0.0.0.0,localhost")
 CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS", default="127.0.0.1")
 
 # Application definition
@@ -20,6 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'django_filters',
     'backend.srvs.office.office',
     'corsheaders',
 ]
