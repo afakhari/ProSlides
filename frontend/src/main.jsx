950a96b40db765be30eb2fdf54ca5f87c3557116
alirezarezaei1 <rly01309@gmail.com>
2025-11-02 17:02:46 +0330

    installing some packages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,6 @@ import { createRoot } from "react-dom/client";
 // import "./index.css";
 import App from "./App.jsx";
 
-const root = createRoot(document.getElementById("rt"));
+const root = createRoot(document.getElementById("root"));
 
 root.render(<App />);
