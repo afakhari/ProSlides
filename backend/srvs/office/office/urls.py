9966be6562f257287c68090afc57150fffcaa5ca
afakhari <hagamirali@gmail.com>
2025-12-22 03:01:22 +0330

    Allow Swagger routes without trailing slash

--- backend/srvs/office/office/urls.py
+++ backend/srvs/office/office/urls.py
@@ -56,11 +56,21 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # API Documentation
-    path('swagger/', schema_view.with_ui('swagger',
-         cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc',
-         cache_timeout=0), name='schema-redoc'),
-    path('swagger.json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(
+        r'^swagger/?$',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'
+    ),
+    re_path(
+        r'^redoc/?$',
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'
+    ),
+    re_path(
+        r'^swagger\\.json/?$',
+        schema_view.without_ui(cache_timeout=0),
+        name='schema-json'
+    ),
 
     # API routes
     path('api/', include(router.urls)),
